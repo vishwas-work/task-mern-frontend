236ef3d095030832abd288b1925efa1d1c0e14ea
vishwas-work <vishwas.dev.work@gmail.com>
2025-02-23 14:56:37 +0530

    Fixed server settings

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = import.meta.env.VITE_API_URL + "/api/users/";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
-
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
   }
-
   return response.data;
 };
 
@@ -19,13 +17,10 @@ const logout = () => {
 const login = async (user) => {
   try {
     const response = await axios.post(`${API_URL}login`, user);
-
     if (response.data?.token) {
-      localStorage.setItem("user", JSON.stringify(response.data)); // Store the entire response
-      // localStorage.setItem("user", response.data); // Store only token
+      localStorage.setItem("user", JSON.stringify(response.data));
     }
-
-    return { success: true, data: response.data }; // Return a consistent object
+    return { success: true, data: response.data };
   } catch (error) {
     throw new Error(error.response?.data?.message || "Something went wrong");
   }
